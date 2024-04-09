693ea913d69688723e46a7e958fb47057bc904f5
rm552329 <isabellajferreira@hotmail.com>
2024-04-09 09:10:13 -0300

    Inclui validações

--- src/main/java/br/com/fiap/springpgdeposito/dto/request/DepositoRequest.java
+++ src/main/java/br/com/fiap/springpgdeposito/dto/request/DepositoRequest.java
@@ -1,9 +1,18 @@
 package br.com.fiap.springpgdeposito.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record DepositoRequest(
+
+
+        @NotNull(message = "O atributo nome é obrigatório")
+        @NotBlank(message = "O atributo nome é obrigatório")
 
         String nome,
 
+
+        @NotNull(message = "O atributo nome é obrigatório")
         EnderecoRequest endereco
 
 
--- src/main/java/br/com/fiap/springpgdeposito/dto/request/ProdutoRequest.java
+++ src/main/java/br/com/fiap/springpgdeposito/dto/request/ProdutoRequest.java
@@ -1,11 +1,25 @@
 package br.com.fiap.springpgdeposito.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.math.BigDecimal;
 
 public record ProdutoRequest(
 
+        @Size(min = 5, max = 255)
+        @NotNull(message = "O nome do produto é obrigatório")
         String nome,
+
+
+        @Size(min = 10, max = 255)
+        @NotNull(message = "A descriçao do produto é obrigatório")
         String descricao,
+
+        @Min(0)
+        @NotNull(message = "O valor do produto é obrigatório")
         BigDecimal valor
 
 ) {
